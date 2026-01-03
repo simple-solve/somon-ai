079f9802105fc6709915e4d7afeedf5acf168242
makhsumovM <muhammadmakhsumov1@gmail.com>
2026-01-03 14:53:52 +0500

    Integrate React Query and add Providers component to RootLayout. Update dependencies to include `@tanstack/react-query` and `next-intl`.

---
+++ frontend/my-app/app/providers/providers.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import React, {ReactNode} from 'react'
+import {QueryClient} from "@tanstack/query-core";
+import {QueryClientProvider} from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient()
+
+const Providers = ({children}: { children: ReactNode }) => {
+
+
+
+  return (
+    <>
+
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+
+    </>
+  )
+}
+export default Providers
